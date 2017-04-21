fb202c85f4c5d4783c3b14b0ae5c88d903bd6929
Refayat Haque <refayathaque@gmail.com>
2017-04-21 19:05:30 -0400

    added more functionality

--- WebFundamentals/JavaScript/ifYouDontMindCanIHaveTheTime.js
+++ WebFundamentals/JavaScript/ifYouDontMindCanIHaveTheTime.js
@@ -1,8 +1,8 @@
-var hour = 3;
-var minute = 48;
+var hour = 6;
+var minute = 5;
 var period = 'AM';
 
-if (minute < 30) {
+if (minute < 30 && minute !== 5) {
   if (period === 'AM') {
     console.log('The time is ' + hour +':' + minute + ' ' + period + '. So it is just after ' + hour + ' in the morning.')
   }
@@ -10,11 +10,35 @@ if (minute < 30) {
     console.log('The time is ' + hour +':' + minute + ' ' + period + '. So it is just after ' + hour + ' in the evening.')
   }
 }
-else if (minute > 30) {
+else if (minute > 30 && minute !== 45) {
   if (period === 'AM') {
     console.log('The time is ' + hour +':' + minute + ' ' + period + '. So it is almost ' + (hour + 1) + ' in the morning.')
   }
   else if (period === 'PM') {
     console.log('The time is ' + hour +':' + minute + ' ' + period + '. So it is almost ' + (hour + 1) + ' in the evening.')
+  }
+}
+else if (minute === 30) {
+  if (period === 'AM') {
+    console.log('The time is ' + hour +':' + minute + ' ' + period + '. So it is half past ' + hour + ' in the morning.')
+  }
+  else if (period === 'PM') {
+    console.log('The time is ' + hour +':' + minute + ' ' + period + '. So it is half past ' + hour + ' in the evening.')
+  }
+}
+else if (minute === 45) {
+  if (period === 'AM') {
+    console.log('The time is ' + hour +':' + minute + ' ' + period + '. So it is quarter after ' + hour + ' in the morning.')
+  }
+  else if (period === 'PM') {
+    console.log('The time is ' + hour +':' + minute + ' ' + period + '. So it is quarter after ' + hour + ' in the evening.')
+  }
+}
+else if (minute === 5) {
+  if (period === 'AM') {
+    console.log('The time is ' + hour +':0' + minute + ' ' + period + '. So it is five after ' + hour + ' in the morning.')
+  }
+  else if (period === 'PM') {
+    console.log('The time is ' + hour +':0' + minute + ' ' + period + '. So it is five after ' + hour + ' in the evening.')
   }
 }
