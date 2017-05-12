d00f14c4a619171a7982d52da786f2316c9f43d2
Refayat Haque <refayathaque@gmail.com>
2017-05-12 17:47:00 -0400

    finished disappearing ninja

--- Python/python_stack/flask_fundamentals/disappearingNinja/server.py
+++ Python/python_stack/flask_fundamentals/disappearingNinja/server.py
@@ -4,25 +4,37 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    #render
     return render_template('index.html')
 
 @app.route('/ninja')
 def ninja():
-    #render
     return render_template('ninja.html')
 
 @app.route('/ninja/<poop>')
 def ninja_color(poop):
-    #render
-    if(poop == 'blue'):
-        image_url = '/static/img/leonardo.jpg'
-    elif(poop == 'purple'):
-        image_url = '/static/img/donatello.jpg'
-    elif(poop == 'red'):
-        image_url = '/static/img/raphael.jpg'
-    elif(poop == 'orange'):
-        image_url = '/static/img/michelangelo.jpg'
-    return render_template('showninja.html', image_url = image_url, view_color = poop)
+    # if(poop == 'blue'):
+    #     image_url = '/static/img/leonardo.jpg'
+    # elif(poop == 'purple'):
+    #     image_url = '/static/img/donatello.jpg'
+    # elif(poop == 'red'):
+    #     image_url = '/static/img/raphael.jpg'
+    # elif(poop == 'orange'):
+    #     image_url = '/static/img/michelangelo.jpg'
+    # return render_template('showninja.html', image_url = image_url, view_color = poop)
+
+    #DICTIONARY method is another way to do what we did above...
+    dictionary = {
+        "blue":"leonardo",
+        "red":"raphael",
+        "orange":"michelangelo",
+        "purple":"donatello"
+    }
+    if poop in dictionary:
+        image_url = "/static/img/" + dictionary[poop] + ".jpg"
+        name_of_turtle = dictionary[poop]
+    else:
+        image_url = "/static/img/notapril.jpg"
+    return render_template("showninja.html", image_url = image_url, view_color = poop, name = name_of_turtle)
+    #format for what's passed to .html ^ - (htmlVAR = pyVAR(MUST BE initatilized WITHIN this method))
 
 app.run(debug=True)
