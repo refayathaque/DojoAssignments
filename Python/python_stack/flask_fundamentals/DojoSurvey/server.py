5323449bd79ac43fcd9b083663cabb86b56b8334
Refayat Haque <refayathaque@gmail.com>
2017-05-13 09:04:14 -0400

    finished DojoSurvey assignment

---
+++ Python/python_stack/flask_fundamentals/DojoSurvey/server.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, redirect, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+@app.route('/results', methods = ['post'])
+def results():
+    name = request.form["name"]
+    dojo_location = request.form["dojo_location"]
+    favorite_language = request.form["favorite_language"]
+    comment = request.form["comment"]
+    return render_template('results.html', name = name, dojo_location = dojo_location, favorite_language = favorite_language, comment = comment)
+
+# #format for what's passed to .html ^ - (htmlVAR = pyVAR(pyVAR MUST BE initatilized WITHIN this method))
+
+@app.route('/return_to_index', methods = ['post'])
+def return_to_index():
+    # return render_template('index.html') - Also works...
+    return redirect('/')
+    # REDIRECT only returning to ROOT (index.html)
+
+app.run(debug=True)
