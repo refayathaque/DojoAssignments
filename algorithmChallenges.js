d9c281b933c5c23f5e25cfd5510a158d947b7475
Refayat Haque <refayathaque@gmail.com>
2017-05-15 10:54:37 -0400

    added a BEAST OF AN ALGO GOD HELP US ALL

--- algorithmChallenges.js
+++ algorithmChallenges.js
@@ -599,9 +599,57 @@ function validParens(str){
   }
   // if(countOpen == 0) return true
   // else return false
-  return countOpen == 0 //new way to do if/else statements, examples ^ & below
-  // return countOpen == 0 ? true : false //ternary operator!
+  return countOpen == 0 //new way to do if/else statements, this is same as ^
+                        //and below(ternary)
+  // return countOpen == 0 ? true : false //TERNARY operator!
 }
 console.log(validParens("(hello))")); //Output: false
+
+console.log('----');
+
+//Braces Valid pg. 53
+function validBraces(str) {
+  var arr = [];
+  for(var i = 0; i < str.length; i++) {
+    if(str[i] == "(" || str[i] == "[" || str[i] == "{") {
+      arr.push(str[i]);
+    }
+    else {
+      if(str[i] == ")") {
+        if(arr[arr.length - 1] == "(") {
+          arr.pop();
+        }
+        else {
+          return false;
+        }
+      }
+      else if(str[i] == "]") {
+        if(arr[arr.length - 1] == "[") {
+          arr.pop();
+        }
+        else {
+          return false;
+        }
+      }
+      else if(str[i] == "}") {
+        if(arr[arr.length - 1] == "{") {
+          arr.pop();
+        }
+        else {
+          return false;
+        }
+      }
+    }
+  }
+  if(arr.length == 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+console.log(validBraces("{{[(hello)]}}")); //Outputs true
+console.log(validBraces("{{[(hello]}}")); //Outputs false
 
 console.log('----');
