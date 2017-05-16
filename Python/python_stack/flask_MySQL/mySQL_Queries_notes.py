783c27d0b176980624cce7b4f7b0ba898e1274a5
Refayat Haque <refayathaque@gmail.com>
2017-05-16 19:31:09 -0400

    SQL is so confusing!

--- Python/python_stack/flask_MySQL/mySQL_Queries_notes.py
+++ Python/python_stack/flask_MySQL/mySQL_Queries_notes.py
@@ -37,3 +37,55 @@
 # UPDATE
 
 # UPDATE table_name SET column_name1 = 'some_value', column_name2='another_value' WHERE condition(s)->(usually id=#)
+
+####
+
+# DELETE
+
+# If you are getting an error regarding SQL SAFE UPDATES, run the following command to let MySQL Workbench know
+# that you know what you are doing and you want to DELETE stuff from the database.
+
+# SET SQL_SAFE_UPDATES = 0;
+
+# The SQL command pattern for deleting/removing records is as follows:
+
+# DELETE FROM table_name WHERE condition(s)
+
+# IMPORTANT: if WHERE condition is not added to the DELETE statement, it will delete all the records on the table.
+
+####
+
+# FUNCTIONS
+
+# When calling a function on a column, make sure that column is the appropriate Data Type for that function.
+
+# Text Functions Data Types (VARCHAR, TEXT, CHAR etc.)
+
+# Numeric Functions Data Types (INT, BIGINT, FLOAT etc.)
+
+# Date and Time Functions Data Types (DATETIME)
+
+# SELECT FUNCTION (column) FROM table_name
+
+# Date Format: https://www.w3schools.com/sql/func_date_format.asp
+
+# HAVING THE SEMI COLON IN THE JOIN STATEMENT IS IMPORTANT!!!! JOIN ;;;;;
+
+####
+
+# JOINS
+
+# When using GROUP BY always use some sort of a function
+
+# As mentioned earlier, LEFT JOIN and JOIN are all that you need for web development.
+
+# When SQL uses the keyword JOIN, it only includes those records that have matches on both sides.
+# It will omit any records that don't have a 'partner'.
+
+# On the other hand, LEFT JOIN will include all the records from the first table (the 'left' table,
+# the first one mentioned when reading from left to right), regardless of whether that record has a
+# matching foreign key in the (right) table that we are trying to join to it.
+
+# To summarize, JOIN will only include the intersection of the two tables, whereas LEFT JOIN will include
+# all records from the first table, plus the records from the second table that correspond. This is why the
+# JOIN is sometimes called the INNER JOIN, while all the other joins (including LEFT JOIN) are referred to as OUTER JOINs. 
