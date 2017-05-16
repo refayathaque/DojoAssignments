0aafe1648c4fe859102dade1ce0340050009158b
Refayat Haque <refayathaque@gmail.com>
2017-05-16 16:41:48 -0400

    updated ninjadojo with help from tims demo

--- Python/python_stack/flask_fundamentals/NinjaGold/server.py
+++ Python/python_stack/flask_fundamentals/NinjaGold/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, session, request, redirect
+from datetime import datetime
 import random
 
 app = Flask(__name__)
@@ -8,6 +9,7 @@ app.secret_key = "SecretKey!"
 @app.route('/')
 def index():
     session['current_gold'] = 0 # To add on to this MUST INITIATE in page load METHOD!
+    session['log'] = []
     return render_template('index.html')
 
 @app.route('/process_money', methods=['POST']) #POST:client submits data AND gets something back (login/username/password)
@@ -16,27 +18,36 @@ def process_money():
         q = random.randrange(10, 21)
         session['current_gold'] += q
         print q
-        return render_template('index.html', earned = "Golds earned from farm: ", golds = q)
+        session['log'].insert(0,('You have earned {} gold from the farm @ {}'.format(q, datetime.now())))
+        return render_template('index.html')
     elif(request.form['building'] == 'cave'):
         q = random.randrange(5, 11)
         session['current_gold'] += q
         print q
-        return render_template('index.html', earned = "Golds earned from cave: ", golds = q)
+        session['log'].insert(0,('You have earned {} gold from the cave @ {}'.format(q, datetime.now())))
+        return render_template('index.html')
     elif(request.form['building'] == 'house'):
         q = random.randrange(2, 6)
         session['current_gold'] += q
         print q
-        return render_template('index.html', earned = "Golds earned from house: ", golds = q)
+        session['log'].insert(0,('You have earned {} gold from the house @ {}'.format(q, datetime.now())))
+        return render_template('index.html')
     elif(request.form['building'] == 'casino'):
-        if(random.randrange(1, 3) == 1):
+        rand_int = random.randrange(1, 3)
+        if(rand_int == 1):
             q = random.randrange(0, 51)
             session['current_gold'] += q
-            return render_template('index.html', earned = "Golds earned from casino: ", golds = q)
-        elif(random.randrange(1, 3) == 2):
+            session['log'].insert(0,('You have earned {} gold from the casino @ {}'.format(q, datetime.now())))
+            return render_template('index.html')
+        elif(rand_int == 2):
             q = random.randrange(0, 51)
             session['current_gold'] -= q
-            return render_template('index.html', earned = "Golds LOST from casino: ", golds = q)
-        print session['current_gold']
-        # return render_template('index.html')
+            session['log'].insert(0,('You have LOST {} gold from the casino @ {}'.format(q, datetime.now())))
+            return render_template('index.html')
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
 
 app.run(debug=True)
