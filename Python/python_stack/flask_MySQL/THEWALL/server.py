be4fc14dcbdea8d089a5f872e84ed81cd711a312
Refayat Haque <refayathaque@gmail.com>
2017-05-20 15:56:47 -0400

    fixed A LOT OF THINGS

--- Python/python_stack/flask_MySQL/THEWALL/server.py
+++ Python/python_stack/flask_MySQL/THEWALL/server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.secret_key = "SecretKey!"
 mysql = MySQLConnector(app,'thewall')
 
-#### functions start ####
+#### FUNCTIONS ####
 
 def info_insertion():
     query = "INSERT INTO users (first_name, last_name, email, password, created_at, updated_at) VALUES (:first_name, :last_name, :email, :password, NOW(), NOW())"
@@ -25,13 +25,15 @@ def friendly_login():
     data = {
         'user_id': session['user_id']
         }
-    logged_in_user = mysql.query_db(query, data)
-    print logged_in_user
-    return render_template("wall.html", logged_in_user=logged_in_user, greeting = 'We are so excited to have you!')
+    user = mysql.query_db(query, data)
+    print user
+    return render_template("wall.html", user = user, greeting = 'We are so excited to have you!')
 
-#### functions end ####
+#### ####
 
-#### routes start ####
+#### ROUTES ####
+
+#### INDEX ####
 
 @app.route('/')
 def index():
@@ -40,46 +42,46 @@ def index():
     # else:
         return render_template('index.html')
 
+#### ####
+
+#### DISPLAYING WALL ####
+
 @app.route('/wall')
 def wall():
     display_messages = mysql.query_db('SELECT message, created_at, updated_at, user_id FROM messages ORDER BY id DESC')
 
     return render_template('wall.html', display_messages = display_messages)
 
+#### ####
 
-
-
-
-
-
-
+#### POPULATING WALL ####
 
 @app.route('/submit_message', methods=['POST'])
 def submit_message():
-    query = "INSERT INTO messages (message, user_id, created_at, updated_at) VALUES (:message, :user_id, :created_at, NOW())"
+    query = "INSERT INTO messages (message, user_id, created_at, created_at_MySQL_form, updated_at) VALUES (:message, :user_id, :created_at, NOW(), NOW())"
+    current_time = datetime.datetime.now().strftime("%B %d %Y at %I:%M %p")
     data = {
         'message': request.form['message'],
-        'user_id': session['user_id'] #FOREIGN KEY! MUST HAVE THIS!
-        'created_at': DATE_FORMAT(NOW(),'%b %d %Y %h:%i %p')
+        'user_id': session['user_id'], #FOREIGN KEY! MUST HAVE THIS!
+        'created_at': current_time #CHANGE from DATETIME in MySQLWorkbench!
         }
     mysql.query_db(query, data)
     # session['user_id'] = mysql.query_db(query, data) # store in session logged in users id
     return redirect('/wall')
 
+#### ####
 
-
-
-
-
-
-
+#### LOGIN ####
 
 @app.route('/login', methods=['POST'])
 def login():
+    # Saving entire list to variable 'es' to use below for validating
+    # email and password
     es = mysql.query_db('SELECT first_name, email, password FROM users')
-    print es
     counter = 0
+    # DON'T TOUCH ^
 
+    # Email validation
     if(len(request.form['email_es']) < 1):
         flash("EMAIL CANNOT BE BLANK!")
     elif not EMAIL_REGEX.match(request.form['email_es']):
@@ -89,7 +91,9 @@ def login():
             if es[i]['email'] == request.form['email_es']:
                 counter += 1
                 break
+                # DON'T TOUCH ^
 
+    # Password validation
     if(len(request.form['password_es']) < 8 and len(request.form['password_es']) >= 1):
         flash("PASSWORD NOT VALID!")
     elif(len(request.form['password_es']) < 1):
@@ -102,22 +106,49 @@ def login():
             elif es[i]['password'] != md5.new(request.form['password_es']).hexdigest():
                 flash("PASSWORD NOT VALID!")
                 break
+                # DON'T TOUCH ^
 
+    # Checking if all validations pass
     if counter == 2:
         flash("You are logged in")
-        query = "SELECT * FROM users WHERE id = :user_id"
+
+        # Getting information of person logging in
+        query = "SELECT * FROM users WHERE email = :email"
         data = {
-        'user_id': session['user_id']
-            }
-        logged_in_user = mysql.query_db(query, data) # ^ gets current user_id! SUPER USEFUL!
-        print logged_in_user
-        return render_template('wall.html', logged_in_user = logged_in_user, greeting = 'Welcome back!')
+            'email': request.form['email_es']
+        }
+        user = mysql.query_db(query, data)
+        # DON'T TOUCH ^
+
+        # Matching email to id then saving id as session[user_id]
+        query = "SELECT id FROM users WHERE email = :email"
+        data = {
+            'email': request.form['email_es']
+        }
+        list_with_one_dictionary_inside = mysql.query_db(query, data)
+        id_of_user_logging_in = list_with_one_dictionary_inside[0]['id']
+        session['user_id'] = id_of_user_logging_in
+        # DON'T TOUCH ^
+
+        # Passing in information of person logging in
+        return render_template('wall.html', user = user, greeting = 'Welcome back!')
+        # DON'T TOUCH ^
+
+    # Staying at login (index) page if validations fail
     else:
         return redirect('/')
+        # DON'T TOUCH ^
+
+#### ####
+
+#### REGISTRATION ####
 
 @app.route('/registration', methods=['POST'])
-def processing():
+def registration():
+
     counter = 0
+
+    # First name validation
     if request.form['first_name'].isalpha() and len(request.form['first_name']) > 2:
         counter += 1
     elif request.form['first_name'].isalpha() and len(request.form['first_name']) < 2:
@@ -125,6 +156,7 @@ def processing():
     elif len(request.form['first_name']) < 1:
         flash('FIRST NAME CANNOT BE BLANK!')
 
+    # Last name validation
     if request.form['last_name'].isalpha() and len(request.form['last_name']) > 2:
         counter += 1
     elif request.form['last_name'].isalpha() and len(request.form['last_name']) < 2:
@@ -132,6 +164,7 @@ def processing():
     elif len(request.form['last_name']) < 1:
         flash('LAST NAME CANNOT BE BLANK!')
 
+    # Email name validation
     if(len(request.form['email']) < 1):
         flash("EMAIL CANNOT BE BLANK!")
     elif not EMAIL_REGEX.match(request.form['email']):
@@ -139,6 +172,7 @@ def processing():
     else:
         counter += 1
 
+    # Password validation
     if(len(request.form['password']) >= 8):
         counter += 1
     elif(len(request.form['password']) < 8 and len(request.form['password']) >= 1):
@@ -146,11 +180,13 @@ def processing():
     elif(len(request.form['password']) < 1):
         flash("PASSWORD CANNOT BE BLANK!")
 
+    # Password match validation
     if(request.form['password'] == request.form['password_confirmation'] and len(request.form['password']) > 0 and len(request.form['password_confirmation']) > 0 ):
         counter += 1
     elif(request.form['password'] != request.form['password_confirmation']):
         flash('PASSWORDS DO NOT MATCH!')
 
+    # Checking if all validations pass...
     if counter == 5:
         flash('You are now a member of TheWall.com')
         info_insertion()
@@ -158,11 +194,18 @@ def processing():
 
     return redirect('/')
 
+#### ####
+
+#### RESET / RETURN TO LOGIN PAGE ####
+
 @app.route('/reset')
 def reset():
+    session.clear()
     return redirect('/')
 
-#### routes end ####
+#### ####
+
+#### ####
 
 app.run(debug=True)
 
---
+++ Python/python_stack/flask_MySQL/THEWALL/thewall.sql
@@ -0,0 +1,4 @@
+select * from messages order by id desc
+
+-- select user_id, message, DATE_FORMAT(created_at,'%b %d %Y %h:%i %p') as created_at from messages order by id desc
+-- 
