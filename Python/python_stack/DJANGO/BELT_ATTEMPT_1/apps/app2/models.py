62439b8aca26fb869b14db2f55954f1a71d81617
Refayat Haque <refayathaque@gmail.com>
2017-05-30 20:41:57 -0400

    FInished corrections on belt exam

--- Python/python_stack/DJANGO/BELT_ATTEMPT_1/apps/app2/models.py
+++ Python/python_stack/DJANGO/BELT_ATTEMPT_1/apps/app2/models.py
@@ -1,51 +1,40 @@
 from __future__ import unicode_literals
-
 from django.db import models
-
-from django.shortcuts import render, HttpResponse, redirect
-
+from django.shortcuts import render, redirect
 from django.contrib import messages
-
 from datetime import datetime
-
-# from django.contrib.sessions.models import Session
-
 from ..app1.models import User
 
 class UserManager(models.Manager):
 
-    def make_trip(self, data):
-
+    def make_trip(self, data, session):
         counter = 0
         errors = []
-
-        # TRIP NAME
         if(len(data['destination']) < 1):
             errors.append('DESTINATION CANNOT BE BLANK!')
             return (False, errors)
-
         elif Trip.objects.filter(destination = data['destination']):
             errors.append('TRIP ALREADY EXISTS!')
             return (False, errors)
-
         else:
             counter += 1
-
-        present = datetime.now()
-        if(data['travel_start'] and data['travel_end'] < present):
-            errors.append('TRIP MUST BE IN THE FUTURE!')
+        if not data['travel_start'] or not data['travel_end']:
+            errors.append('FILL OUT DATE FIELDS!')
             return (False, errors)
-        elif(data['travel_end'] < data['travel_start']):
-            errors.append('TRIP END MUST BE AFTER START!')
+        startdate = datetime.strptime(data['travel_start'], '%Y-%m-%d')
+        enddate = datetime.strptime(data['travel_end'], '%Y-%m-%d')
+        if enddate < startdate:
+            errors.append('IMPLAUSIBLE DATES!')
+            return (False, errors)
+        elif startdate < datetime.now():
+            errors.append('TRIP MUST BE START IN THE FUTURE!')
             return (False, errors)
         else:
             counter += 1
-
         if counter == 2:
-            Trip.objects.create(created_by = data['created_by'], destination = data['destination'], travel_start = data['travel_start'], travel_end = data['travel_end'], description = data['description'])
+            Trip.objects.create(created_by = session, destination = data['destination'], travel_start = data['travel_start'], travel_end = data['travel_end'], description = data['description'])
             return (True, data['destination']) # need session here to get username
 
-# TABLES
 class Trip(models.Model):
     destination = models.CharField(max_length=100)
     created_by = models.CharField(max_length=100)
@@ -58,10 +47,8 @@ class Trip(models.Model):
     def __str__(self):
         return self.destination + " " + self.travel_start + " " + self.travel_end + " " + self.description
 
-# JOINED TABLE
 class User_Trip(models.Model):
     trip_id = models.ForeignKey(Trip, related_name="trip_name")
     user_id = models.ForeignKey(User, related_name="user_name")
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
-####
