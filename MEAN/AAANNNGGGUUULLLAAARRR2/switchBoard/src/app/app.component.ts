09bad1b219e61d1f5d3b89e20a16f39097b6012c
Refayat Haque <refayathaque@gmail.com>
2017-06-14 16:57:42 -0400

    Finished Switchboard Assignment

--- MEAN/AAANNNGGGUUULLLAAARRR2/switchBoard/src/app/app.component.ts
+++ MEAN/AAANNNGGGUUULLLAAARRR2/switchBoard/src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Switchboard';
+
+  count1: number = 0;
+  count2: number = 0;
+  count3: number = 0;
+  count4: number = 0;
+
+  toggle1(){
+    this.count1++
+    console.log('Button has been toggled!')
+  }
+  toggle2(){
+    this.count2++
+    console.log('Button has been toggled!')
+  }
+  toggle3(){
+    this.count3++
+    console.log('Button has been toggled!')
+  }
+  toggle4(){
+    this.count4++
+    console.log('Button has been toggled!')
+  }
 }
