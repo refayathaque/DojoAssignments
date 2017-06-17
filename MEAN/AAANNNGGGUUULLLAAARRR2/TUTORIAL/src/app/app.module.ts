e1586ddb5625e2c6ca8fddc7115dd3749d235afe
Refayat Haque <refayathaque@gmail.com>
2017-06-17 11:40:24 -0400

    Began working on Architecture part of Angular2 Tutorial

--- MEAN/AAANNNGGGUUULLLAAARRR2/TUTORIAL/src/app/app.module.ts
+++ MEAN/AAANNNGGGUUULLLAAARRR2/TUTORIAL/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
+import { GrandchildComponent } from './child/grandchild/grandchild.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent,
+    GrandchildComponent
   ],
   imports: [
     BrowserModule,
