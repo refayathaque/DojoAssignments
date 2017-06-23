6c2b56adc7549aa253bf4429240439db75e9a821
Refayat Haque <refayathaque@gmail.com>
2017-06-22 21:01:00 -0400

    Finished Angular2 Tutorial

--- MEAN/AAANNNGGGUUULLLAAARRR2/TUTORIAL/src/app/app-routing.module.ts
+++ MEAN/AAANNNGGGUUULLLAAARRR2/TUTORIAL/src/app/app-routing.module.ts
@@ -5,13 +5,18 @@ import { OfficeComponent } from './office/office.component';
 import { TaskComponent } from './task/task.component';
 import { ObsChildOneComponent } from './obs-child-one/obs-child-one.component';
 import { ObsChildTwoComponent } from './obs-child-two/obs-child-two.component';
+import { GrandchildComponent } from './child/grandchild/grandchild.component';
+import { ChildComponent } from './child/child.component';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'office', component: OfficeComponent },
     { path: 'task/:id', component: TaskComponent },
     { path: 'obs-child-one', component: ObsChildOneComponent },
-    { path: 'obs-child-two', component: ObsChildTwoComponent }
+    { path: 'obs-child-two', component: ObsChildTwoComponent },
+    { path: 'child', component: ChildComponent, children:[
+		{ path: 'grandchild', component: GrandchildComponent }
+    ]}
 ];
 
 @NgModule({
