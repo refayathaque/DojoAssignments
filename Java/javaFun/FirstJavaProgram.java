306bfe24736617ee6ba67ac4fcc3cc286f9880be
Refayat Haque <refayathaque@gmail.com>
2017-07-05 11:12:46 -0400

    FirstJavaProgram

---
+++ Java/javaFun/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Refayat Haque");
+        System.out.println("I am 27 years old");
+        System.out.println("My hometown is Dhaka, Bangladesh");
+    }
+}
---
+++ Java/javaFun/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+// This is the SOURCE FILE
+// Content inside this file is the SOURCE CODE
+// DOUBLE QUOTATIONS! Single quotations may give errors
+// The HelloWorld.class contains BYTECODE
+// Case Sensitivity - Java is case sensitive, which means "Hello" and "hello" have a different meaning
+// Class names are written in PascalCase, also known as UpperCamelCase
+// Method Names - All method names should start with a lower case letter. If there are multiple words in the method, they should be written in lowerCamelCase. Typically, we use verbs as method names, rather than noun statements
+// Program Filename - The name of the program file should exactly match the class name
