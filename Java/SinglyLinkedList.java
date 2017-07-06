389804cfe2824eb3a68e09981862acb7c289e752
Refayat Haque <refayathaque@gmail.com>
2017-07-06 18:19:30 -0400

    SinglyLinkedList

---
+++ Java/SinglyLinkedList.java
@@ -0,0 +1,56 @@
+public class SinglyLinkedList{
+    private Node head;
+
+    public SinglyLinkedList(){this.head = null;}
+
+    public Node add(int value){
+        if(this.head == null){
+            this.head = new Node(value);
+            return this.head;
+        }
+        Node ptr = this.head;
+
+        while(ptr.next != null){
+            ptr = ptr.next;
+        }
+
+        ptr.next = new Node(value);
+        return this.head;
+    }
+
+    public Node remove(){
+        Node ptr = this.head.next;
+        Node prev = this.head;
+
+        while(ptr != null){
+            if(ptr.next != null){
+                ptr = ptr.next;
+                prev = prev.next;
+            }else{
+                prev.next = null;
+            }
+        }
+        return this.head;
+    }
+
+    public void printValues() {
+        Node ptr = this.head;
+
+        while(ptr != null) {
+            System.out.println(ptr.value);
+
+            ptr = ptr.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList SLL = new SinglyLinkedList();
+        SLL.add(1);
+        SLL.add(2);
+        SLL.add(3);
+        SLL.add(4);
+
+        // SLL = SLL.remove();
+        SLL.printValues();
+    }
+}
