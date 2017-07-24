b09380587b1354a0e565d82a86d29b692e052c11
Refayat Haque <refayathaque@gmail.com>
2017-07-24 19:52:45 -0400

    Implemented Named Routes, Messages in progress

--- Python/DJANGO2/Log_Reg_Msgs/apps/log_reg_app/views.py
+++ Python/DJANGO2/Log_Reg_Msgs/apps/log_reg_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import User
 from django.contrib import messages
+from django.core.urlresolvers import reverse
 
 def index(request):
     return render(request, 'log_reg_app/index.html')
@@ -9,16 +10,18 @@ def registration(request):
     validation_tuple = User.objects.registration_validation(request.POST)
     if validation_tuple[0] == True:
         request.session['user_id'] = User.objects.get(email=validation_tuple[1]).id # ORM METHOD to GET OTHER FIELD values IN ROW using value of ONE FIELD
+        print '*** CREATED & LOGGEDIN! SESSION ID : ', request.session['user_id']
     else:
         for error in validation_tuple[1]:
             messages.error(request, error)
-    return redirect('/')
+    return redirect(reverse('log_reg:index'))
 
 def login(request):
     validation_tuple = User.objects.login_validation(request.POST)
     if validation_tuple[0] == True:
         request.session['user_id'] = User.objects.get(email = validation_tuple[1]).id
+        print '*** LOGGEDIN! SESSION ID : ', request.session['user_id']
     else:
         for error in validation_tuple[1]:
             messages.error(request, error)
-    return redirect('/')
+    return redirect(reverse('log_reg:index'))
