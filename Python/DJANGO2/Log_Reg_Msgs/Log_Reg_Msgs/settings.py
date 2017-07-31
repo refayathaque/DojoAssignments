ca742a8ad08f26bd05c38d8ed1b6027312fd1154
Refayat Haque <refayathaque@gmail.com>
2017-07-31 10:17:33 -0400

    Messages back up

--- Python/DJANGO2/Log_Reg_Msgs/Log_Reg_Msgs/settings.py
+++ Python/DJANGO2/Log_Reg_Msgs/Log_Reg_Msgs/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    "../apps/log_reg_app/static",
+    "../apps/msgs_app/static"
+]
+# ^ KEY to get CSS working well
