64b3ec2d424872047ec8ee201f105eccb390eb5f
Refayat Haque <refayathaque@gmail.com>
2017-08-13 14:17:51 -0400

    algz

--- MEAN/practice/algz.js
+++ MEAN/practice/algz.js
@@ -48,3 +48,27 @@ function fizzbuzz() {
 }
 
 fizzbuzz();
+
+console.log('----')
+
+function fizzbuzz2() {
+    for(i = 1; i <= 100; i++) {
+        if(i % 3 === 0 && i % 5 === 0) {
+            console.log('fizzbuzz')
+            continue;
+        }
+        if(i % 3 === 0) {
+            console.log('fizz')
+            continue;
+        }
+        if(i % 5 === 0) {
+            console.log('buzz')
+            continue;
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
+
+fizzbuzz2();
