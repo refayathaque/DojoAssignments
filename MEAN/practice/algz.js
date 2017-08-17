92ddea98ca3dc7496f623b6b62f3f24ac0ad726e
Refayat Haque <refayathaque@gmail.com>
2017-08-17 13:50:01 -0400

    Daily FizzBuzz Practice

--- MEAN/practice/algz.js
+++ MEAN/practice/algz.js
@@ -98,3 +98,27 @@ function fizzbuzz3() {
 }
 
 fizzbuzz3();
+
+console.log('---------------------------------------')
+
+function fizzbuzz4() {
+    for(i = 1; i <= 100; i++) {
+        if(i % 3 === 0 && i % 5 === 0) {
+            console.log('fizzbuzz')
+            continue;
+        }
+        if(i % 3 === 0) {
+            console.log('fizz')
+            continue;
+        }
+        if(i % 5 === 0) {
+            console.log('buzz')
+            continue;
+        }
+        else {
+            console.log(i)
+        }
+    }
+}
+
+fizzbuzz4();
